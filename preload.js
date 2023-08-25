4428896ac4466cd410d375318a30e198bbea11f6
Ruben Tanahara <rmartinez@gdlconnect.com>
2023-08-24 20:08:23 -0600

    new boiler plate

--- preload.js
+++ preload.js
@@ -1,18 +1,13 @@
-const { contextBridge, ipcRenderer } = require("electron");
+window.addEventListener("DOMContentLoaded", () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  };
 
-contextBridge.exposeInMainWorld("versions", {
-  node: () => process.versions.node,
-  //node is the version of node that we are using
-  chrome: () => process.versions.chrome,
-  // chrome is the version of chrome that we are using
-  electron: () => process.versions.electron,
-  //electron is the version of electron that we are using
-  ping: () => ipcRenderer.invoke("ping"),
-  // ping is a function that will invoke the ping function in main.js
-  // we can also expose variables, not just functions
-});
-
-/*
+  for (const type of ["chrome", "node", "electron"]) {
+    replaceText(`${type}-version`, process.versions[type]);
+  }
+}); /*
  *
 A preload script contains code that runs before your web page is loaded into the browser window. It has access to both DOM APIs and Node.js environment, and is often used to expose privileged APIs to the renderer via the contextBridge API.
 
